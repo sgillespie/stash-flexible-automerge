390a47768ade204492c10610d0599bc9d5de5659
Sean D Gillespie <sean@mistersg.net>
2014-10-14 13:26:34 -0400

    Add JUnit test skeleton

---
+++ src/test/java/ut/com/github/sgillespie/stash/hook/FlexibleAutomergePostReceiveHookTest.java
@@ -0,0 +1,13 @@
+package ut.com.github.sgillespie.stash.hook;
+
+import org.junit.Test;
+
+/**
+ * Created by sgillespie on 10/14/14.
+ */
+public class FlexibleAutomergePostReceiveHookTest {
+    @Test
+    public void test() {
+
+    }
+}
