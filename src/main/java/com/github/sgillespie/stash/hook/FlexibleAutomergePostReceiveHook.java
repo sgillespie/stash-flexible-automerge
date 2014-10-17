94fec983c98345d45d84b6295ac19783c2a00353
Sean D Gillespie <sean@mistersg.net>
2014-10-17 09:22:12 -0400

    Delete autogenerated code

--- src/main/java/com/github/sgillespie/stash/hook/FlexibleAutomergePostReceiveHook.java
+++ src/main/java/com/github/sgillespie/stash/hook/FlexibleAutomergePostReceiveHook.java
@@ -12,28 +12,10 @@ public class FlexibleAutomergePostReceiveHook implements AsyncPostReceiveReposit
      * Connects to a configured URL to notify of all changes.
      */
     @Override
-    public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges)
-    {
-        String url = context.getSettings().getString("url");
-        if (url != null)
-        {
-            try
-            {
-                new URL(url).openConnection().getInputStream().close();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
+    public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
     }
 
     @Override
-    public void validate(Settings settings, SettingsValidationErrors errors, Repository repository)
-    {
-        if (settings.getString("url", "").isEmpty())
-        {
-            errors.addFieldError("url", "Url field is blank, please supply one");
-        }
+    public void validate(Settings settings, SettingsValidationErrors errors, Repository repository) {
     }
 }
--- src/test/java/ut/com/github/sgillespie/stash/hook/FlexibleAutomergePostReceiveHookTest.java
+++ src/test/java/ut/com/github/sgillespie/stash/hook/FlexibleAutomergePostReceiveHookTest.java
@@ -1,13 +1,29 @@
 package ut.com.github.sgillespie.stash.hook;
 
+import com.atlassian.stash.hook.repository.RepositoryHookContext;
+import com.atlassian.stash.repository.RefChange;
+import com.github.sgillespie.stash.hook.FlexibleAutomergePostReceiveHook;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
-/**
- * Created by sgillespie on 10/14/14.
- */
+import static java.util.Arrays.asList;
+
 public class FlexibleAutomergePostReceiveHookTest {
-    @Test
-    public void test() {
+    private FlexibleAutomergePostReceiveHook flexibleAutomergePostReceiveHook;
 
+    @Mock
+    private RepositoryHookContext repositoryHookContext;
+    @Mock
+    private RefChange refChange;
+
+    @Before
+    public void setUp() {
+        flexibleAutomergePostReceiveHook = new FlexibleAutomergePostReceiveHook();
+    }
+
+    @Test
+    public void pushToReleaseBranchShouldTriggerAutomerge() {
+        flexibleAutomergePostReceiveHook.postReceive(repositoryHookContext, asList(refChange));
     }
 }
