a9041b4e801c57431bdf6028dbf586d828aa5c87
Sean D Gillespie <sean@mistersg.net>
2014-10-21 11:52:55 -0400

    Add branch model domain classes

---
+++ src/main/java/com/github/sgillespie/stash/hook/domain/HierarchicalBranch.java
@@ -0,0 +1,48 @@
+package com.github.sgillespie.stash.hook.domain;
+
+import com.atlassian.stash.repository.Branch;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class HierarchicalBranch implements Branch {
+    private final Branch branch;
+    private Collection<HierarchicalBranch> parents;
+
+    public HierarchicalBranch(Branch branch) {
+        this.branch = branch;
+        this.parents = new ArrayList<HierarchicalBranch>();
+    }
+
+    @Override
+    public boolean getIsDefault() {
+        return branch.getIsDefault();
+    }
+
+    @Nonnull
+    @Override
+    public String getLatestChangeset() {
+        return branch.getLatestChangeset();
+    }
+
+    @Nonnull
+    @Override
+    public String getDisplayId() {
+        return branch.getDisplayId();
+    }
+
+    @Nonnull
+    @Override
+    public String getId() {
+        return branch.getId();
+    }
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public Collection<HierarchicalBranch> getParents() {
+        return parents;
+    }
+}
---
+++ src/main/java/com/github/sgillespie/stash/hook/domain/HierarchicalBranchModel.java
@@ -0,0 +1,16 @@
+package com.github.sgillespie.stash.hook.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class HierarchicalBranchModel {
+    private Collection<HierarchicalBranch> branches = new ArrayList<HierarchicalBranch>();
+
+    public Collection<HierarchicalBranch> getBranches() {
+        return branches;
+    }
+
+    public void setBranches(Collection<HierarchicalBranch> branches) {
+        this.branches = branches;
+    }
+}
